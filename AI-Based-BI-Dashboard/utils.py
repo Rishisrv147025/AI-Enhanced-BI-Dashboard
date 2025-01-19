92fa69414c39b37631d7b88c7468551762d2771c
Rishisrv147025 <rishisrv147025@gmail.com>
2025-01-19 15:36:39 +0530

    Delete AI-Based-BI-Dashboard directory

--- AI-Based-BI-Dashboard/utils.py
+++
@@ -1,8 +0,0 @@
-# List of available operations for querying
-def get_operations_list():
-    return [
-        "max", "min", "sum", "difference", "top", "mean", "average",
-        "groupby", "filter", "sort", "median", "count", "pivot",
-        "join", "merge", "null", "range", "date", "trend", "normalize",
-        "bin", "aggregate", "unique", "standardize"
-    ]
